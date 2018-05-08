5758bf69acd3e29a8d3975c546095a30614d3bf6
schoonmt@gmail.com <schoonmt@gmail.com>
2018-05-08 09:08:16 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,23 @@
+const musicians = ["John", "Paul", "George", "Ringo"];
+const instruments = ["guitar", "guitar", "bass", "drums"]
+
+function theBeatlesPlay(musicians, instruments) {
+  var array = []
+  for (let i = 0; i < 4; i++) {
+    array.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return array
+}
+
+const facts = [ "He was the last Beatle to learn to drive", 
+"He was never a vegetarian", 
+"He was a choir boy and a boy scout", 
+"He hated the sound of his own voice"];
+function johnLennonFacts(facts) {
+  let i = 0
+  while (i < facts.length) {
+    facts.push(`${facts}!!!`)
+    i++
+  }
+  return facts;
+}
